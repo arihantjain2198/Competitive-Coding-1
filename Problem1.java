1fd8bcafcb283835a1a99c9ba1f89ec307fdb89c
arihantjain2198 <31105987+arihantjain2198@users.noreply.github.com>
2025-06-17 19:16:13 -0700

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,23 @@
+class solution {
 
+    static int missingNumber(int[] nums) {
+        int n = nums.length + 1;
+
+        // Iterate from 1 to n and check
+        // if the current number is present
+        for (int i = 1; i <= n; i++) {
+            boolean found = false;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] == i) {
+                    found = true;
+                    break;
+                }
+            }
+
+            // If the current number is not present
+            if (!found)
+                return i;
+        }
+        return -1;
+    }
+}
